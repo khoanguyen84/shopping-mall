e25c0d1ba01bdbab5dca0cc80a01bfc2c0fc41de
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-01-24 16:10:00 +0700

    update template

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,31 +5,32 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div className="container d-flex align-items-center py-4 border-bottom">
-            <div className="d-flex align-items-center" style={{ minWidth: "230px" }}>
-                <Link to={"/"} className="logo">
-                    <SiKasasmart size={30} className="me-2" /> 
-                    <span className="fs-4">Shopping Mall</span>
-                </Link>
-            </div>
-            <div className="d-flex flex-grow-1 justify-content-between">
-                <form className="w-50 d-flex align-items-center">
-                    <input
-                        type="search"
-                        placeholder="Enter product name"
-                        className="form-control"
-                        style={{ paddingRight: '25px' }}
-                    />
-                    <FaSearch size={15} style={{ marginLeft: '-25px', color: 'rgba(0,0,0,.2)' }} />
-                </form>
-                {/* <div className="">
+        <div className="container py-4 border-bottom">
+            <div className="row">
+                <div className="col-sm-12 col-md-4 col-lg-2">
+                    <Link to={"/"} className="logo">
+                        <SiKasasmart size={30} className="me-2" />
+                        <span className="fs-4">Shopping Mall</span>
+                    </Link>
+                </div>
+                <div className="col-sm-12 col-md-8 col-lg-10">
+                    <form className="w-50 d-flex align-items-center">
+                        <input
+                            type="search"
+                            placeholder="Enter product name"
+                            className="form-control"
+                            style={{ paddingRight: '25px' }}
+                        />
+                        <FaSearch size={15} style={{ marginLeft: '-25px', color: 'rgba(0,0,0,.2)' }} />
+                    </form>
+                    {/* <div className="">
                     <Link to={'/cart'}>
                         <FaShoppingCart size={20} className="me-2" role="button" />
                     </Link>
                     <FaUser size={20} role="button" />
                 </div> */}
+                </div>
             </div>
-
         </div>
     )
 }
